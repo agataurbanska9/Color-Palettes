0b7bb2a7632675ee4fe3525327f022fc15a0d6ff
Agata Urbańska <agata@agatau.home>
2020-05-14 15:39:42 +0200

    added to surge.sh

--- App/js/App.js
+++ App/js/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {PlantData} from "./PlantData";
 import {ChooseColorSection} from "./ChooseColorSection";
 import '../css/style.css';
 
